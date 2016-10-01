70c04ee2a1ac34a9d0ac8fbaa1f92710dcf2edf5
Sagang Wee <sagangwee@yahoo.com>
2016-10-01 11:40:48 -0700

    Added Medieval Action Sound FX Pack

---
+++ Assets/Scripts/StabMotion.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StabMotion : MonoBehaviour {
+
+	// Given new coordinates from player phone input
+	public Vector3 newPhoneValues;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (StabTransition ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator StabTransition () {
+		float duration = 3.0f;
+
+		Vector3 startPosition = this.transform.position;
+		Vector3 endPosition = new Vector3 (newPhoneValues.x, newPhoneValues.y, newPhoneValues.z);
+
+		for (float i = 0; i < duration; i += Time.deltaTime) {
+			Vector3 newPosition = Vector3.Lerp(startPosition, endPosition, i / duration);
+			this.transform.position = newPosition;
+
+			// pauses coroutine until next frame
+			yield return null;
+		}
+
+		// sets final position of arm to the end position
+		this.transform.position = endPosition;
+	}
+}
