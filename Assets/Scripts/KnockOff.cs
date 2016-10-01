35847f4e3a9d982912b575ec7c585d3bb3ea8d4e
Sagang Wee <sagangwee@yahoo.com>
2016-10-01 12:09:26 -0700

    Collision impact script

---
+++ Assets/Scripts/KnockOff.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnockOff : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+
+		float force = 10.0;
+
+		if (other.tag == "Player" || other.tag == "Enemy") {
+			// Calculate Angle Between the collision point and the player
+			Vector3 dir = other.contacts[0].point - transform.position;
+			// We then get the opposite (-Vector3) and normalize it
+			dir = -dir.normalized;
+			// And finally we add force in the direction of dir and multiply it by force. 
+			// This will push back the player
+			GetComponent<Rigidbody>().AddForce(dir*force);
+		}
+	}
+}
