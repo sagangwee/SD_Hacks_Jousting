93bef2675ee77e3dd7c51bdee81ab349f1382d1f
Sagang Wee <sagangwee@yahoo.com>
2016-10-01 20:00:46 -0700

    Improved Angle of Enemy Stab

---
+++ Assets/Scripts/EnemyStabbing.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStabbing : MonoBehaviour {
+
+	public GameObject enemyPlayer;
+	static Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = enemyPlayer.GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+        if (other.tag == "StabPoint") {
+            animator.SetTrigger ("isAttacking");
+        }
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,16 @@ public class GameController : MonoBehaviour {
 	public GameObject enemySpear;
 
 	void Start () {
+		moveSpearRelativeToHand();
+	}
+
+	void Update () {
+		moveSpearRelativeToHand();
+		enemySpear.transform.localRotation = Quaternion.Euler (49.711f, -1.971f, 33.494f);
+	}
+
+	void moveSpearRelativeToHand () {
 		enemySpear.transform.SetParent(enemyBonePosition.transform);
+		enemySpear.transform.localPosition = new Vector3(0, 0, 0);
 	}
 }
