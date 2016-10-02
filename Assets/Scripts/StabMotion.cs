6c21e297cf0cae6a17d0c64b012ae873a42e109a
Owen Jow <owenjow@berkeley.edu>
2016-10-02 01:28:01 -0700

    Linked motion values to controls, fixed (or at least improved) lance movement

--- Assets/Scripts/StabMotion.cs
+++ Assets/Scripts/StabMotion.cs
@@ -2,35 +2,34 @@
 using System.Collections;
 
 public class StabMotion : MonoBehaviour {
-
-	// Given new coordinates from player phone input
-	public Vector3 newPhoneValues;
+	public GameObject tracking_client_go;
+	private TrackingClient tracking_client;
+	public float sensitivity = 120f;
+	private Quaternion _targetRotation = Quaternion.identity;
+	private float prev_ori_x, prev_ori_y, prev_ori_z;
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (StabTransition ());
+		tracking_client = tracking_client_go.GetComponent<TrackingClient>();
 	}
-	
+
+	public void SetBlendedEulerAngles(Vector3 angles) {
+		_targetRotation = Quaternion.Euler(angles);
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
-	}
+		Vector3 new_euler_angles = new Vector3(
+			transform.eulerAngles.x - (tracking_client.orientation_y - prev_ori_y),
+			transform.eulerAngles.y + (tracking_client.orientation_z - prev_ori_z),
+			transform.eulerAngles.z - (tracking_client.orientation_x - prev_ori_x)
+		);
+		prev_ori_x = tracking_client.orientation_x;
+		prev_ori_y = tracking_client.orientation_y;
+		prev_ori_z = tracking_client.orientation_z;
+		SetBlendedEulerAngles(new_euler_angles);
 
-	IEnumerator StabTransition () {
-		float duration = 3.0f;
-
-		Vector3 startPosition = this.transform.position;
-		Vector3 endPosition = new Vector3 (newPhoneValues.x, newPhoneValues.y, newPhoneValues.z);
-
-		for (float i = 0; i < duration; i += Time.deltaTime) {
-			Vector3 newPosition = Vector3.Lerp(startPosition, endPosition, i / duration);
-			this.transform.position = newPosition;
-
-			// pauses coroutine until next frame
-			yield return null;
-		}
-
-		// sets final position of arm to the end position
-		this.transform.position = endPosition;
+		transform.rotation = Quaternion.RotateTowards(transform.rotation,
+				_targetRotation, sensitivity);
 	}
 }
