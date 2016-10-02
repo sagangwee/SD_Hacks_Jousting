fb40c9f3953223c466e1eaa627d4ff975261f46b
Sagang Wee <sagangwee@yahoo.com>
2016-10-01 23:58:14 -0700

    Broke Horse animation

--- Assets/Scripts/HorseMover.cs
+++ Assets/Scripts/HorseMover.cs
@@ -4,9 +4,27 @@ using System.Collections;
 public class HorseMover : MonoBehaviour {
 
 	public Transform target;
+	public Transform endTarget;
 	public float speed;
+	public float endSpeed;
+
+	private static Animator animator;
+	private bool reachedEnd;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
 	void Update() {
 		float step = speed * Time.deltaTime;
 		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+		if (transform.position == target.position) {
+			reachedEnd = true;
+			animator.SetTrigger ("isWalking");
+		}
+		if (reachedEnd) {
+			float smallStep = endSpeed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, endTarget.position, smallStep);
+		}
 	}
 }
